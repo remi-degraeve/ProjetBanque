11fe30e0d67a8a5048f5a6718971401612978e93
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2022-11-15 13:56:03 +0100

    dd

---
+++ Class/Class/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+
+class TextRectangle {
+
+private:
+
+	int width;
+	int height;
+
+public:
+
+	int setWidth(int);
+	int setHeight(int);
+	int getArea();
+	int getPerimeter();
+	void print();
+};
+
+
+
+int main(){
+
+}
