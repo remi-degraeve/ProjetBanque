cc9ed92608dc3258b32f1bf20ae9c74f73df1bd3
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2022-11-21 10:03:13 +0100

    v

---
+++ ProjectB/ProjectB/ProjectB.cpp
@@ -0,0 +1,12 @@
+﻿// ProjectB.cpp : définit le point d'entrée de l'application.
+//
+
+#include "ProjectB.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
