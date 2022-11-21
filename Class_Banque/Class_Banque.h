9ca90317608359a5481561710ceb5087f64eadeb
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2022-11-21 09:57:29 +0100

    Delete Class_Banque directory

--- Class_Banque/Class_Banque.h
+++
@@ -1,8 +0,0 @@
-﻿// First_world_bank.h : fichier Include pour les fichiers Include système standard,
-// ou les fichiers Include spécifiques aux projets.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
