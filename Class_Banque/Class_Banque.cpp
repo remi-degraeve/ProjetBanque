e1a778c7dc6d3081bf3737035b0d95e8626c57a5
MatthieuBaum <90194395+MatthieuBaum@users.noreply.github.com>
2022-11-21 09:33:04 +0100

    Add files via upload

---
+++ Class_Banque/Class_Banque.cpp
@@ -0,0 +1,12 @@
+﻿// First_world_bank.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Class_Banque.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
